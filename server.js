53614576c070a30b087a2b606ce049bd3daf2b8f
BHAWNACLEAR <azar.shaikh3113@gmail.com>
2026-09-07 15:02:24 +0530

    update server.js2

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const express = require('express');
-const app = express(); // Yeh line hamesha sabse upar honi chahiye
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
-
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
-// Ab yahan safe tareeqe se app.use use karein
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
